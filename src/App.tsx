ac4f7985fa225d2358a4c63e0a94f7cbb6fe9995
PabloViniegra <pablovpmadrid@gmail.com>
2026-07-04 10:20:52 +0200

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@heroui/react'
+
+function App() {
+
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold">Audio Analyzer</h1>
+        <Button>Click me</Button>
+      </section>
+    </>
+  )
+}
+
+export default App
